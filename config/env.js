2bfc06c007c32ff355c7f930244ba91626159c65
Edgar Garcia <edgargarciafcb@gmail.com>
2024-11-04 14:50:31 -0600

    Inicializando el repositorio

---
+++ config/env.js
@@ -0,0 +1,16 @@
+
+const env = {
+  database: 'examen_final_jeg3',
+  username: 'examen_final_jeg3_user',
+  password: 'SyPLNsSfh3vn8ZDS2mVpIRrHxADi4QD9',
+  host: 'dpg-cskidfq3esus7384jlhg-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+  }
+};
+
+module.exports = env;
